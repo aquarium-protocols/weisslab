e4daf02eedfb80144e93ecccfd47182573f88454
deemishra <dmishra@mit.edu>
2017-03-13 01:41:24 -0400

    Update CHO_transfection.rb

--- workflows/mammalian/CHO_transfection.rb
+++ workflows/mammalian/CHO_transfection.rb
@@ -23,9 +23,9 @@ class Protocol
     io_hash = input if !input[:io_hash] || input[:io_hash].empty?
     
      # setup default values for io_hash.
-    io_hash = { tube_ids: [], Plasmids: [], debug_mode: "No" }.merge io_hash
+    io_hash = { tube_ids: [], Plasmids: [], item_choice_mode: "No" debug_mode: "No" }.merge io_hash
     
-    io_hash[:plasmid_ids] = io_hash[:Plasmids]
+    io_hash[:plasmid_stock_ids] = io_hash[:Plasmids]
     
     
     if io_hash[:debug_mode].downcase == "yes"
@@ -34,26 +34,24 @@ class Protocol
       end
     end
     
-    err_messages = []
-
-    plasmid_stocks = io_hash[:plasmid_ids].collect { |ids|
-      ids.collect { |id|
-        err_messages.push "Sample #{id} does not have any stock" if !find(:sample,{ id: id })[0].in("Plasmid Stock")[0]
-        find(:sample,{ id: id })[0].in("Plasmid Stock")[0]
+     if io_hash[:plasmid_stock_ids].length == 0
+      show {
+        title "No plasmid transfection required"
+        note "No transfection required. Thanks for you effort!"
       }
-    }
-    
-    uniq_plasmid_stocks = plasmid_stocks.flatten.uniq
-    
+      return { io_hash: io_hash }
+    end
+
+    plasmid_stocks = io_hash[:plasmid_stock_ids].collect{ |pid| find(:item, id: pid )[0] }
+        
     show {
       title "CHO Transfection"
       note "This protocol will perform a co-transfection of CHO-K1 cells using Viafect with the following plasmids."
-      note io_hash[:Plasmids].collect { |id| "#{id}" }
+      note io_hash[:plasmid_stocks].collect { |id| "#{id}" }
       }
     
-    stocks = uniq_plasmid_stocks;
-    
-    take stocks, interactive: true, method: "boxes"
+      
+    take plasmid_stocks, interactive: true, method: "boxes"
     
   end
 end
