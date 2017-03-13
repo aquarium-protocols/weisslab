fded7dce94adbd3dd94af2b7e1d52df4658b3f8f
deemishra <dmishra@mit.edu>
2017-03-13 01:23:38 -0400

    Update CHO_transfection.rb

--- workflows/mammalian/CHO_transfection.rb
+++ workflows/mammalian/CHO_transfection.rb
@@ -55,43 +55,4 @@ class Protocol
     
     take stocks, interactive: true, method: "boxes"
 
-    plasmids = stocks.collect { |p| p.sample }
-    tubes = produce spread plasmids, "1.7mL Tube", 1, 12
-
-    # assuming 1800 ng of total DNA and 50uL reaction
-    show {
-      title "Prepare Tubes"
-      tubes.each do |tu|
-        check "Label a new tube with the id #{tu}. "
-        end
-    }
-    
-    plasmid_stocks_volumes = stocks.collect { |p| (300.0/(p.datum[:concentration]||300)).round(1) }
-    plasmid_stocks_volumes_list = plasmid_stocks_volumes.zip(stocks).collect { |a| a.join(" µL of ") }
-    water_volume_list = plasmid_stocks_volumes.collect { |v| “#{48.5 – v} uL” }
-    load_samples_variable_vol( ["Plasmid", "MG Water"], [plasmid_stocks_volumes_list,  water_volume_list ], tubes) {
-      warning "Use a fresh pipette tip for each transfer."
-    }
-
-      show {
-	        title "Add Viafect and Incubation"
-	        note "Add 1.5 µL of Viafect to each tube"
-	        tubes.each do |tu|
-	          bullet "Tubes #{tu.id}, numbers #{tu.non_empty_string}"
-	        end
-        	timer initial: { hours: 0, minutes: 15, seconds: 0}
-
-	      }
-
-    
-# Set tasks in the io_hash to be transfected
-    if io_hash[:task_ids]
-      io_hash[:task_ids].each do |tid|
-        task = find(:task, id: tid)[0]
-        set_task_status(task,"transfected")
-      end
-    end
-    return { io_hash: io_hash }
-
-  end
 end
