baeef53dfebfd11d85aaf88a287bacf4d7b2f613
deemishra <dmishra@mit.edu>
2017-03-13 03:10:26 -0400

    Update CHO_Color_transfection.rb

--- workflows/mammalian/CHO_Color_transfection.rb
+++ workflows/mammalian/CHO_Color_transfection.rb
@@ -29,10 +29,10 @@ class Protocol
     show {
       title "CHO Transfection"
       note "This protocol will perform a co-transfection of CHO-K1 cells using Viafect with constitutive color plasmids."
-      check "Well 1 is a negative control"
-      check "Well 2 is a single color eBFP"
-      check "Well 3 is a dual color eBFP/eYFP"
-      check "Well 4 is a triple color eBFP/eYFP/mKate"
+      check "Well 1 is a single color eBFP"
+      check "Well 2 is a dual color eBFP/eYFP"
+      check "Well 3 is a triple color eBFP/eYFP/mKate"
+      check "Well 4 is a negative control"
       }
     
     # take plasmids, interactive: true, method: "boxes"
@@ -40,20 +40,23 @@ class Protocol
     
     show {
       title "Labeling Plastic Ware"
-      note "Obtain a 24-well plate from the TAs and label with your initials, CHO0, and 3/13/2017"
-      note "Get 4 1.7mL Eppendorfs and label them 1, 2, 3, and 4."
+      warning "Ensure you stay sterile with all items coming into the TC hood!"
+      check "Obtain a 24-well plate from the TAs and label with your initials, CHO0, and 3/13/2017"
+      check "Get 4 1.7mL Eppendorfs and label them 1, 2, 3, and 4."
       }
     
     show {
       title "Standardizing wells of 24-well plate volume"
-      note "Aspirate media from each of four wells, being careful to come slowly on the side of the well"
-      note "Add 0.5mL of pre-warmed media to each well"
+      warning "The seeding of your plates may have had slightly variable amounts of media. Do not skip this step!"
+      check "Aspirate media from each of four wells, being careful to come slowly on the side of the well"
+      check "Add 0.5mL of pre-warmed media to each well"
       }
     
     show {
       title "Preparing Transfection Complexes"
-      note "Obtain plasmids 3000, 3001, 3002, and 3003, all of whom are in 150ng/uL aliquots"
+      note "Obtain plasmids 3000, 3001, 3002, and 3003, all are in 150ng/uL aliquots"
       note "Pipette the following tube combinations:"
+      warning "Be aware of the plasmid identifies AND different volumes"
       check "2uL of 3000 into Tube 1"
       check "6uL of 3003 into Tube 1"
       
@@ -74,12 +77,13 @@ class Protocol
       title "Preparing Transfection Complexes"
       check "Pipette 40.5uL of Optimem Serum-Free Media into Tubes 1, 2, 3 and 4."
       check "Pipette 1.5uL of Viafect into Tubes 1, 2, 3, and 4."
-      check "Swirl to mix and proceed."
+      check "Swirl tube to mix and proceed."
       }
     
     show {
       title "Incubation Step for Complexes at Room-Temperature"
-       timer initial: { hours: 0, minutes: 15, seconds: 0}
+      note "Incubate complexes for 12 minutes and click OK when timer ends."
+       timer initial: { hours: 0, minutes: 12, seconds: 0}
       }
     
     show {
@@ -93,6 +97,8 @@ class Protocol
     show {
       title "Clean and Store"
       check "Wipe plate with EtOH and return plate to incubator for growth"
+      check "Dispose of 1.7mL eppendorfs in biowaste."
+      check "Clean hood"
       }
     
     
