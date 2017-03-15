4a54f6baecca79b3aaa1230b334c23e6bc97e799
deemishra <dmishra@mit.edu>
2017-03-15 12:08:28 -0400

    Update CHO_Color_transfection.rb

--- workflows/mammalian/CHO_Color_transfection.rb
+++ workflows/mammalian/CHO_Color_transfection.rb
@@ -28,11 +28,10 @@ class Protocol
         
     show {
       title "CHO Transfection"
-      note "This protocol will perform a co-transfection of CHO-K1 cells using Viafect with constitutive color plasmids."
-      check "Well 1 is a single color eBFP"
-      check "Well 2 is a dual color eBFP/eYFP"
-      check "Well 3 is a triple color eBFP/eYFP/mKate"
-      check "Well 4 is a negative control"
+      note "This protocol will perform a co-transfection of CHO-K1 cells using Viafect with an inducible TRE-Tight/rtTA3 plasmids."
+      check "Well 1 is a -DOX inducible mKate with const eYFP"
+      check "Well 2 is a +DOX inducible mKate with const eYFP"
+      check "Well 3 is a negative transfection control"
       }
     
     # take plasmids, interactive: true, method: "boxes"
@@ -41,42 +40,35 @@ class Protocol
     show {
       title "Labeling Plastic Ware"
       warning "Ensure you stay sterile with all items coming into the TC hood!"
-      check "Obtain a 24-well plate from the TAs and label with your initials, CHO0, and 3/13/2017"
-      check "Get 4 1.7mL Eppendorfs and label them 1, 2, 3, and 4."
+      check "Obtain a 24-well plate from the TAs and label with your initials, CHO1, and 3/15/2017"
+      check "Get 3 1.7mL Eppendorfs and label them 1, 2, and 3."
       }
     
-    show {
-      title "Standardizing wells of 24-well plate volume"
-      warning "The seeding of your plates may have had slightly variable amounts of media. Do not skip this step!"
-      check "Aspirate media from each of four wells, being careful to come slowly on the side of the well"
-      check "Add 0.5mL of pre-warmed media to each well"
-      }
     
     show {
       title "Preparing Transfection Complexes"
-      note "Obtain plasmids 3000, 3001, 3002, and 3003, all are in 150ng/uL aliquots"
+      note "Obtain plasmids 3004, 3005, 3006, and 3007, all are in 150ng/uL aliquots"
       note "Pipette the following tube combinations:"
       warning "Be aware of the plasmid identifies AND different volumes"
-      check "2uL of 3000 into Tube 1"
-      check "6uL of 3003 into Tube 1"
+      check "2uL of 3004 into Tube 1"
+      check "2uL of 3005 into Tube 1"
+      check "2uL of 3006 into Tube 1"
+      check "2uL of 3007 into Tube 1"
       
-      check "2uL of 3000 into Tube 2"
-      check "2uL of 3001 into Tube 2"
-      check "4uL of 3003 into Tube 2"
+      check "2uL of 3004 into Tube 2"
+      check "2uL of 3005 into Tube 2"
+      check "2uL of 3006 into Tube 2"
+      check "2uL of 3007 into Tube 2"
       
-      check "2uL of 3000 into Tube 3"
-      check "2uL of 3001 into Tube 3"
-      check "2uL of 3002 into Tube 3"
-      check "2uL of 3003 into Tube 3"
-      
-      check "8uL of 3003 into Tube 4"
+      check "2uL of 3006 into Tube 3"
+      check "6uL of 3007 into Tube 3"
       }
     
         
     show {
       title "Preparing Transfection Complexes"
-      check "Pipette 40.5uL of Optimem Serum-Free Media into Tubes 1, 2, 3 and 4."
-      check "Pipette 1.5uL of Viafect into Tubes 1, 2, 3, and 4."
+      check "Pipette 40.5uL of Optimem Serum-Free Media into Tubes 1, 2, and 3."
+      check "Pipette 1.5uL of Viafect into Tubes 1, 2, and 3."
       check "Swirl tube to mix and proceed."
       }
     
@@ -88,10 +80,9 @@ class Protocol
     
     show {
       title "Transfection"
-      check "Pipette 50uL of Tube 1 into Well A1"
-      check "Pipette 50uL of Tube 2 into Well B1"
-      check "Pipette 50uL of Tube 3 into Well C1"
-      check "Pipette 50uL of Tube 4 into Well D1"
+      check "Pipette 50uL of Tube 1 into Well A6"
+      check "Pipette 50uL of Tube 2 into Well B6"
+      check "Pipette 50uL of Tube 3 into Well C6"
       }
     
     show {
